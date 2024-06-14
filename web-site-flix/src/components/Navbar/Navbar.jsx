a7e68bce98ac2149f2178f02bdc63db5009fd297
IgorNakajima2006 <157403610+IgorNakajima2006@users.noreply.github.com>
2024-06-14 14:37:52 -0300

    .

--- web-site-flix/src/components/Navbar/Navbar.jsx
+++ web-site-flix/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import search_icon from '../../assets/search_icon.svg'
 import bell_icon from '../../assets/bell_icon.svg'
 import profile_img from '../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
-import { logout } from '../../firebase'
 
 const Navbar = () => {
   const navRef = useRef();
@@ -42,7 +41,7 @@ const Navbar = () => {
         <img src={profile_img} alt=""  className='profile'/>
         <img src={caret_icon} alt=""/>
         <div className="dropdown">
-        <p onClick={()=>{logout}}>Sair do App</p>
+        <p>Sair do App</p>
         </div>
 
         </div>
--- web-site-flix/src/pages/Login/Login.jsx
+++ web-site-flix/src/pages/Login/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './Login.css'
 import logo from '../../assets/logo.png'
-import { login, signup } from '../../firebase'
 import netflix_spinner from '../../assets/netflix_spinner.gif'
 
 
